cf6c4facf1218b28d12a2790b5f39098b0580497
Hbest1804 <hnguyenh8201@gmail.com>
2026-06-10 10:02:19 +0700

    fix(frontend): add location and key to Routes to fix AnimatePresence exit animations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
         <Header />
         
         <AnimatePresence mode="wait">
-          <Routes>
+          <Routes location={location} {...({ key: location.pathname } as any)}>
             <Route
               path="/"
               element={
